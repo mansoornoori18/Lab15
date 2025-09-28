5f5411c52dfb9fbd8f20879bce1ac124c1f7314e
mansoornoori18 <mnoori926@insite.4cd.edu>
2025-09-27 21:56:04 -0700

    update

--- Lab15.cpp
+++ Lab15.cpp
@@ -1,4 +1,15 @@
-#include<iostream>
+#include<iostream>  // COMSC-210 | Lab 15 | Mansoor Noori
+#include<string>    // IDE Visual Studio,
+#include<fstream>   // for file input
+
+// Movie class definition
+class Movie {
+    private:
+    string title;      // Movie title
+    int 
+
+};
+
 
 using namespace std;
 
@@ -9,5 +20,5 @@ int main(){
     cout << "hello" << endl;
 
     return 0;
-    
+
 }
