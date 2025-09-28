d12fded3ee70a99d956efcf8a68794c078c905b6
mansoornoori18 <mnoori926@insite.4cd.edu>
2025-09-27 22:08:18 -0700

    update

--- Lab15.cpp
+++ Lab15.cpp
@@ -2,11 +2,14 @@
 #include<string>    // IDE Visual Studio,
 #include<fstream>   // for file input
 
+using namespace std;
+
 // Movie class definition
 class Movie {
     private:
-    string title;      // Movie title
-    int 
+    string title;                  // Movie title
+    int yearReleased;              // Year the movie was released
+    string screenWriter;           // Screenwriter's name
 
 };
 
